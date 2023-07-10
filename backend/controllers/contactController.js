ef524564e8f2f7c48115bb8c631984b123c9d99c
Tuấn Nghĩa <tuannghia20062003@gmmail.com>
2023-07-10 22:49:17 +0700

    Add async

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,7 +1,7 @@
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
-const getContacts = (req, res) => {
+const getContacts = async (req, res) => {
     res.status(200).json({ message: "Get all contacts" })
 }
 
